fd9236fe89f3476137eb0fb84a44d811b215b7c8
AnonAlly <193170968+AnonAlly@users.noreply.github.com>
2025-08-05 12:39:07 +0200

    Change

--- test.js
+++ test.js
@@ -5,26 +5,30 @@ browser.trial.ml.onProgress.addListener(progressData => {
   // 2. Création du moteur Q&A
   async function initQAEngine() {
     await browser.trial.ml.createEngine({
-      taskName: "question-answering:",
+      taskName: "translation",
       modelHub: "huggingface"
     });
   }
   
   // 3. Fonction qui interroge le moteur avec un contexte et une question
-  async function answerQuestion(questionText, contextText) {
+  async function answerQuestion(sentenceText) {
     const response = await browser.trial.ml.runEngine({
-      args: [questionText, contextText]
+      args: [
+        sentenceText, {
+          tgt_lang: 'en', // English
+        }
+      ]
     });
     // response est un tableau, chaque élément contient answer, score, start, end
-    const result = response[0]["generated_text"];
+    const result = response[0]["translation_text"];
     return result;
   }
   
   // Exemple d’usage
   (async () => {
     await initQAEngine();
-    const context = "Le mont blanc fait 1000km de long et 20 de large.";
-    const question = "Quelle taille en longueur fait le mont blanc ?";
-    const res = await answerQuestion(question, context);
+
+    const sentence = "Quelle taille en longueur fait le mont blanc ?";
+    const res = await answerQuestion(sentence);
     console.log(`Réponse: ${res}`);
   })();
